fe10f0525f68a95661eea2ea6e0074060be01946
Nacho Valera <nacho.valera@devopensource.com>
2017-05-20 22:54:53 +0200

    Beginning users module

---
+++ codesynthesis-backend/server/modules/users/controller.js
@@ -0,0 +1,11 @@
+import User from './model'
+
+export const loginWithAuth0 = async (req, res) => {
+    const { ...args } = req.body
+    try {
+        const user = await User.create(args)
+        return res.status(200).json({ user })
+    } catch(e) {
+        return res.status(400).json({ error: true, errorMessage: 'Oups! Something went wrong.' })
+    }
+}
